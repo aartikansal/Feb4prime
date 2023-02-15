e77f50ce32423376033556bfb0aaa90841a7d34b
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-15 17:55:34 -0500

    Check prime number

--- src/MyNumber.java
+++ src/MyNumber.java
@@ -1,9 +1,13 @@
 public class MyNumber {
-
-    public MyNumber(int i) {
+    private int number;
+    public MyNumber(int number) {
+        this.number = number;
     }
 
     public Boolean isPrime() {
+        // check if a number is divisible by 2
+        for(int i = 2; i<= number -1; i++)
         return false;
+        return true;
     }
 }
