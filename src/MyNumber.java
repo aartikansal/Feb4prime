82b0e10c5317fe97832f3931629f7decbb5638b1
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-15 18:00:30 -0500

    Check prime number, add condition and guard condition

--- src/MyNumber.java
+++ src/MyNumber.java
@@ -1,13 +1,19 @@
 public class MyNumber {
     private int number;
+
     public MyNumber(int number) {
         this.number = number;
     }
 
     public Boolean isPrime() {
         // check if a number is divisible by 2
-        for(int i = 2; i<= number -1; i++){
-            if (number % i == 0){
+        //add guard condition
+        if (number < 2) {
+            return false;
+
+        }
+        for (int i = 2; i <= number - 1; i++) {
+            if (number % i == 0) {
                 return false;
 
             }
